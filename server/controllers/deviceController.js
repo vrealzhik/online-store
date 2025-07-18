e14a42d6ca3ce3f659a97cc750033a6214cb3142
VrealZhik <you@example.com>
2025-07-18 13:23:45 +0300

    [+] controlers, middleware, routes

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,15 @@
+class DeviceController {
+    async create(req, res) {
+
+    }
+
+    async getAll(res, req) {
+
+    }
+
+    async getOne(req, res) {
+        
+    }
+}
+
+module.exports = new DeviceController()
