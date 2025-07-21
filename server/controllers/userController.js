3c65d8040ff43162361278be10ff92914fa6233b
VrealZhik <you@example.com>
2025-07-21 13:53:45 +0300

    [+] check auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -44,11 +44,8 @@ class UserController {
   }
 
   async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("Не задан ID"));
-    }
-    res.json(id);
+    const tokens = generateJwt(req.user.id, req.user.email, req.user.role)
+    return res.json({token})
   }
 }
 
